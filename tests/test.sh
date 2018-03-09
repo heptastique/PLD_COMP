3354afc41793bcf95769b68f7a82afcb89c511b1
Victor <vb4007@gmail.com>
2018-03-09 10:00:48 +0100

    Test indication

--- tests/test.sh
+++ tests/test.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 . lib/assert.sh
 
+# How to test
+# assert "<command>" "<expected command output>"
+
 assert "../bin/app Custom/simple-expr" "5"
 assert "../bin/app Custom/big-expr" "20"
 
