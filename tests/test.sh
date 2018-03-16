56a107fc0600bbef6b56901539671f20b76a77fe
Victor <vb4007@gmail.com>
2018-03-16 20:20:55 +0100

    Actually execute the test

--- tests/test.sh
+++ tests/test.sh
@@ -4,5 +4,7 @@
 # How to test
 # assert "<command>" "<expected command output>"
 
+assert "../bin/app Custom/01-simple.c" "Programme :\n Declaration: Name=unChar Type=3\n Fonction: Name=main\n     Bloc=\n\nOk"
+
 # end of test suite
 assert_end examples
