de0dce78608cd247b748923b5f2eb5f582ad408f
AmosTrask <vincent.redoute@live.fr>
2018-03-18 14:21:02 +0100

    first test

--- tests/UnitTests/UnitTest.cpp
+++ tests/UnitTests/UnitTest.cpp
@@ -1,13 +1,20 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "../lib/catch.hpp"
+#include "../../src/Bloc.h"
+#include <string>
+#include <iostream>
+#include "../../src/Type.h"
+#include "../../src/Declaration.h"
 
-unsigned int Factorial( unsigned int number ) {
-    return number <= 1 ? number : Factorial(number-1)*number;
+std::string AddInstruction(){
+    Bloc* b = new Bloc();
+    Instruction * inst = new Declaration("name",CHAR);
+    b->addInstruction(inst);
+    std::ostringstream stream;
+    stream << *b;
+    return stream.str();
 }
 
-TEST_CASE( "Factorials are computed", "[factorial]" ) {
-    REQUIRE( Factorial(1) == 1 );
-    REQUIRE( Factorial(2) == 2 );
-    REQUIRE( Factorial(3) == 6 );
-    REQUIRE( Factorial(10) == 3628800 );
+TEST_CASE("Instruction is added","[bloc]" ) {
+    REQUIRE ( AddInstruction() == "     Bloc:\n     Instruction: Declaration: Name=name Type=3\n\n");
 }
