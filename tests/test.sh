56484088118d37fb25aec81139545fbf00bda3f9
Vincent Redouté <vincent.redoute@live.fr>
2018-03-18 15:14:19 +0100

    separating tests

--- tests/test.sh
+++ tests/test.sh
@@ -4,12 +4,17 @@
 # How to test
 # assert "<command>" "<expected command output>"
 
+# Custom tests
+
 assert "../bin/app Custom/01-simple-func.c" "Programme:\n Fonction: Name=main\n     Bloc:\n\n\nOk"
 assert "../bin/app Custom/03-decl-func.c" "Programme:\n Declaration: Name=a Type=3\n Fonction: Name=main\n     Bloc:\n\n\nOk"
 assert "../bin/app Custom/04-multiple-decl.c" "Programme:\n Declaration: Name=a Type=3\n Declaration: Name=b Type=3\n Fonction: Name=main\n     Bloc:\n\n\nOk"
-assert "../bin/app Front/ValidPrograms/01_SimpleDeclaration.c" "Programme:\n Fonction: Name=main\n     Bloc:\n     Instruction: Declaration: Name=a Type=3\n\n\nOk"
 assert "../bin/app Custom/06-simple-decl-func-bloc.c" "Programme:\n Declaration: Name=a Type=3\n Fonction: Name=main\n     Bloc:\n     Instruction: Declaration: Name=b Type=3\n\n\nOk"
 assert "../bin/app Custom/07-simple-decl-func-bloc-mult.c" "Programme:\n Declaration: Name=a Type=3\n Fonction: Name=main\n     Bloc:\n     Instruction: Declaration: Name=b Type=3\n     Instruction: Declaration: Name=i Type=1\n\n\nOk"
+
+# Given tests
+
+assert "../bin/app Front/ValidPrograms/01_SimpleDeclaration.c" "Programme:\n Fonction: Name=main\n     Bloc:\n     Instruction: Declaration: Name=a Type=3\n\n\nOk"
 
 # end of test suite
 assert_end examples
