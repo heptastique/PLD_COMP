f6409b3067af59739d66271391de2ab3b8852482
Vincent Redouté <vincent.redoute@live.fr>
2018-03-18 15:06:25 +0100

    new tests

--- tests/test.sh
+++ tests/test.sh
@@ -7,6 +7,8 @@
 assert "../bin/app Custom/01-simple-func.c" "Programme:\n Fonction: Name=main\n     Bloc:\n\n\nOk"
 assert "../bin/app Custom/03-decl-func.c" "Programme:\n Declaration: Name=a Type=3\n Fonction: Name=main\n     Bloc:\n\n\nOk"
 assert "../bin/app Custom/04-multiple-decl.c" "Programme:\n Declaration: Name=a Type=3\n Declaration: Name=b Type=3\n Fonction: Name=main\n     Bloc:\n\n\nOk"
+assert "../bin/app Custom/05-simple-func-bloc.c" "Programme:\n Fonction: Name=main\n     Bloc:\n     Instruction: Declaration: Name=a Type=3\n\n\nOk"
+assert "../bin/app Custom/06-simple-decl-func-bloc.c" "Programme:\n Declaration: Name=a Type=3\n Fonction: Name=main\n     Bloc:\n     Instruction: Declaration: Name=b Type=3\n\n\nOk"
 
 # end of test suite
 assert_end examples
