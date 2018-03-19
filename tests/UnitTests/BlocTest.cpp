376d62ac84eeede0973e9153b9bc86961d7a5aac
AmosTrask <vincent.redoute@live.fr>
2018-03-19 22:27:41 +0100

    new unittest

--- tests/UnitTests/BlocTest.cpp
+++ tests/UnitTests/BlocTest.cpp
@@ -1,20 +1,20 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "../lib/catch.hpp"
 #include "../../src/Bloc.h"
-#include <string>
-#include <iostream>
 #include "../../src/Type.h"
 #include "../../src/Declaration.h"
 
-std::string AddInstruction(){
+Instruction* AddInstruction(Instruction* inst){
     Bloc* b = new Bloc();
-    Instruction * inst = new Declaration("name",CHAR);
+
     b->addInstruction(inst);
-    std::ostringstream stream;
-    stream << *b;
-    return stream.str();
+
+    std::list<Instruction*> list = b->getInstructions();
+
+    return list.front();
 }
 
 TEST_CASE("Instruction is added","[bloc]" ) {
-    REQUIRE ( AddInstruction() == "     Bloc:\n      Instruction: Declaration: Name=name Type=3\n\n");
+    Instruction * inst = new Declaration("name",CHAR);
+    REQUIRE ( AddInstruction(inst) == inst);
 }
