bb1af0ebe61146eab4aaee5010517c8198cec7a0
Victor <vb4007@gmail.com>
2018-03-20 14:30:28 +0100

    Added return types tests

--- tests/test.sh
+++ tests/test.sh
@@ -15,6 +15,9 @@ assert "../bin/app Custom/08-simple-func-param.c" "Programme:\n Fonction: Name=m
 assert "../bin/app Custom/09-simple-func-mult-param.c" "Programme:\n Fonction: Name=main TypeRetour=0\n     Param:\n      Declaration: Name=n Type=3\n      Declaration: Name=i Type=1\n     Bloc:\n\n\nOk"
 assert "../bin/app Custom/10-decl-func-param-bloc.c" "Programme:\n Declaration: Name=a Type=3\n Declaration: Name=leInt Type=2\n Fonction: Name=main TypeRetour=0\n     Param:\n      Declaration: Name=c Type=3\n      Declaration: Name=oint Type=1\n      Declaration: Name=bigInt Type=2\n     Bloc:\n      Instruction: Declaration: Name=b Type=3\n      Instruction: Declaration: Name=i Type=1\n\n\nOk"
 assert "../bin/app Custom/11-multiple-func.c" "Programme:\n Fonction: Name=test1 TypeRetour=0\n     Bloc:\n\n Fonction: Name=main TypeRetour=0\n     Bloc:\n\n\nOk"
+assert "../bin/app Custom/12-return-int32.c" "Programme:\n Fonction: Name=func TypeRetour=1\n     Bloc:\n\n\nOk"
+assert "../bin/app Custom/13-return-int64.c" "Programme:\n Fonction: Name=func TypeRetour=2\n     Bloc:\n\n\nOk"
+assert "../bin/app Custom/14-return-char.c" "Programme:\n Fonction: Name=func TypeRetour=3\n     Bloc:\n\n\nOk"
 
 # Given tests
 
