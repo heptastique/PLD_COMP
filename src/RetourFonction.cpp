f16b799c21143653ba53ecd82004621a864f2c73
osubou <oussama.boukich@insa-lyon.fr>
2018-03-30 11:31:30 +0200

    return function: generateIR()

--- src/RetourFonction.cpp
+++ src/RetourFonction.cpp
@@ -3,6 +3,22 @@ using namespace std;
 #include "RetourFonction.h"
 #include <iostream>
 
+
+
+void RetourFonction::generateIR(ControlFlowGraph * controlFlowGraph){
+	cout << "RetouFonction::generateIR" << endl;
+	string retValue = createNewReg();
+	list <string> params;
+	
+	params.push_back(retValue);
+	params.push_back(expr);
+	controlFlowGraph->addInstruction(IRInstr(FUNCTION_RETURN), params);
+	
+	return retValue;
+	
+	
+}
+
 void RetourFonction::print(std::ostream &stream) const
 {
     stream << " RetourFonction: " << *expression;
@@ -19,7 +35,7 @@ std::ostream& operator<<(std::ostream& stream, const RetourFonction& retourFonct
 RetourFonction &RetourFonction::operator=(const RetourFonction &unRetourFonction) {
 }
 
-void RetourFonction::resolveScopeVariables(std::vector<Declaration*> declProgramme, std::vector<Declaration*> paramFunction, std::vector<Declaration*> declBloc, std::vector<Function*> functionProgram){
+void RetourFonction::resolveScopeVariables(std::list<Declaration*> declProgramme, std::list<Declaration*> paramFunction, std::list<Declaration*> declBloc, std::list<Function*> functionProgram){
     this->expression->resolveScopeVariables(declProgramme, paramFunction, declBloc, functionProgram);
 }
 
