d39eb685a5c60af8106722462e9d6fba9b370a08
M4p4ch3 <etienne.raquin@insa-lyon.fr>
2018-04-07 11:02:43 +0200

    Delete Useless, Rename Some Mnemos

--- src/IRInstr.h
+++ src/IRInstr.h
@@ -11,34 +11,36 @@ class ControlFlowGraph;
 
 enum Mnemonique
 {
-    DECL = 0,
-    RET = 1,
+    // Function
+    PROLOG = 0,
+    EPILOG = 1,
     CALL = 2,
 
-    REG_STORE = 3,
-    STORE_RBP_REL = 4,
-    PUSH_VALUE = 5,
-    PUSH_RBP_REL = 6,
+    // MOV
+    MOV_VALUE_RBP_REL = 10,
+    MOV_REG_RBP_REL = 11,
+    MOV_RBP_REL_REG = 12,
+    MOV_RBP_REL_RBP_REl = 13,
 
-    ADD_RSP = 10,
-    SUB_RSP = 11,
+    // Stack
+    PUSH_VALUE = 20,
+    PUSH_RBP_REL = 21,
+    ADD_RSP = 22,
+    SUB_RSP = 23,
 
-    MOV_REG_RBP_REL = 20,
-    MOV_RBP_REL_REG = 21,
-
+    // Conditions
     COMPJUMP = 30,
-    LABEL = 31,
-    RETIF = 32,
-    COMPJUMPEQUALS = 33,
+    COMPJUMPEQUALS = 31,
+    LABEL = 32,
+    RETIF = 33,
 
-    AFFECTATION = 40,
+    // Operations
     BINARYOPERATION= 41,
     UNARYOPERATION= 42,
-    ADD = 13,
-    PREINCIR = 50,
-    PREDECIR = 51,
-    POSINCIR = 52,
-    POSDECIR = 53,
+    PREINCIR = 43,
+    PREDECIR = 44,
+    POSINCIR = 45,
+    POSDECIR = 46,
 };
 
 class IRInstr
